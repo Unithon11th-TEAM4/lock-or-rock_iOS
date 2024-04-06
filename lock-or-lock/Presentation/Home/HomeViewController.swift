29bb65a959e80bead42b45331c5ca6d5a3803a37
devjoonn <phj3046@gmail.com>
2024-04-06 13:40:30 +0900

    First Init

---
+++ lock-or-lock/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  lock-or-lock
+//
+//  Created by 박현준 on 4/6/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
