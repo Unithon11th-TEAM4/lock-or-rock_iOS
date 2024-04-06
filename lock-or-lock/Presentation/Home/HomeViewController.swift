6a52873013ac7ceb0a507236947eed100c7b9cef
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-04-06 21:42:50 +0900

    feat: 홈 UI 일부 구현

--- lock-or-lock/Presentation/Home/HomeViewController.swift
+++ lock-or-lock/Presentation/Home/HomeViewController.swift
@@ -11,9 +11,15 @@ import SnapKit
 
 class HomeViewController: UIViewController {
     
-    var leaderBoardStackView: UIStackView!
-
-    private let imageView = UIImageView()
+    private let cautionImage = UIImageView().then {
+        $0.image = UIImage(named: "caution")
+        $0.contentMode = .scaleToFill
+    }
+    
+    private let devilImage = UIImageView().then {
+        $0.image = UIImage(named: "devil")
+        $0.contentMode = .scaleToFill
+    }
     
     func configurationButton(_ button: UIButton, action: Selector) {
         button.backgroundColor = UIColor(named: "primary")
@@ -26,65 +32,75 @@ class HomeViewController: UIViewController {
         button.addTarget(self, action: action, for: .touchUpInside)
     }
     
-    lazy var viewLeaderBoardButton = UIButton().then {
-        $0.setTitle("리더보드 보기", for: .normal)
-        configurationButton($0, action: #selector(viewleaderBoardButtonTapped))
+    private let containerView = UIView().then {
+        $0.backgroundColor = .white
+        
     }
     
-    lazy var startQuestionButton = UIButton().then {
+    private let startQuestionButton = UIButton().then {
         $0.setTitle("퀴즈 풀기", for: .normal)
-        configurationButton($0, action: #selector(startQuestionButtonTapped))
+        $0.addTarget(self, action: #selector(startQuestionButtonTapped), for: .touchUpInside)
     }
-    lazy var proposeQuestionButton = UIButton().then {
-        configurationButton($0, action: #selector(startQuestionButtonTapped))
+    
+    private let proposeQuestionButton = UIButton().then {
         $0.setTitle("퀴즈 제안하기", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.borderColor = UIColor(named: "black")?.cgColor
+        $0.addTarget(self, action: #selector(proposeQuestionButtonTapped), for: .touchUpInside)
+    }
+    
+    private let viewRankgingButton = UIButton().then {
+        $0.setTitle("랭킹 확인하기", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.layer.cornerRadius = 12
+        $0.layer.borderWidth = 3
+        $0.layer.borderColor = UIColor(named: "black")?.cgColor
+        $0.addTarget(self, action: #selector(viewRankgingButtonTapped), for: .touchUpInside)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .black
         
         addSubViews()
         setupLayout()
     }
     
     private func addSubViews() {
-        leaderBoardStackView = 
-        UIStackView(arrangedSubviews: [imageView, viewLeaderBoardButton]).then({
-            $0.axis = .horizontal
-            $0.spacing = 5
-        })
-        
-        [leaderBoardStackView, startQuestionButton, proposeQuestionButton].forEach { view.addSubview($0) }
+        [cautionImage, devilImage, startQuestionButton, proposeQuestionButton, viewRankgingButton].forEach { view.addSubview($0) }
+        devilImage.bringSubviewToFront(cautionImage)
     }
     
     private func setupLayout() {
-        leaderBoardStackView.snp.makeConstraints {
+        cautionImage.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
             $0.centerX.equalToSuperview()
-            $0.leading.equalTo(view.snp.leading).offset(20)
-            $0.trailing.equalTo(view.snp.trailing).offset(20)
-            $0.top.equalTo(view.snp.top).offset(40)
-            $0.height.equalTo(71)
+        }
+        
+        devilImage.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.centerX.equalToSuperview()
         }
         
         startQuestionButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.leading.equalTo(view.snp.leading).offset(50)
-            $0.top.equalTo(leaderBoardStackView.snp.bottom).offset(30)
-            $0.height.equalTo(169)
+            $0.leading.equalTo(view.snp.leading).offset(15)
+            $0.top.equalTo(view.snp.bottom).offset(296)
+            $0.height.equalTo(245)
         }
         
         proposeQuestionButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
+            $0.leading.equalTo(view.snp.leading).offset(15)
+            $0.top.equalTo(startQuestionButton.snp.bottom).offset(30)
+        }
+        
+        viewRankgingButton.snp.makeConstraints {
             $0.leading.equalTo(view.snp.leading).offset(50)
             $0.top.equalTo(startQuestionButton.snp.bottom).offset(30)
             $0.height.equalTo(169)
         }
-    }
-    
-    @objc func viewleaderBoardButtonTapped() {
-//        let leaderBoardVC = leaderBoardViewController()
-//        self.navigationController?.pushViewController(leaderBoardVC, animated: true)
     }
     
     @objc func startQuestionButtonTapped() {
@@ -95,5 +111,10 @@ class HomeViewController: UIViewController {
     @objc func proposeQuestionButtonTapped() {
 //        let proposeQuizVC = proposeQuizViewController()
 //        self.navigationController?.pushViewController(proposeQuizVC, animated: true)
+    }
+    
+    @objc func viewRankgingButtonTapped() {
+//        let leaderBoardVC = leaderBoardViewController()
+//        self.navigationController?.pushViewController(leaderBoardVC, animated: true)
     }
 }
